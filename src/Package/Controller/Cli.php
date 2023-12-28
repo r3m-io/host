5351d625402eb9057148050b0985d4fa83bfdcf6
remco van der Velde <remco.pc@outlook.com>
2023-12-28 22:34:20 +0100

    host & host.mapper

--- src/Package/Controller/Cli.php
+++ src/Package/Controller/Cli.php
@@ -24,9 +24,9 @@ class Cli extends Controller {
     const INFO = [
         '{{binary()}} r3m_io/host                    | Host options',
         '{{binary()}} r3m_io/host setup              | Host setup',
-        '{{binary()}} r3m_io/host name add           | Host name add',
-        '{{binary()}} r3m_io/host name delete        | Host name delete',
-        '{{binary()}} r3m_io/host name has           | Host name has',
+        '{{binary()}} r3m_io/host name add           | /etc/hosts add',
+        '{{binary()}} r3m_io/host name delete        | /etc/hosts delete',
+        '{{binary()}} r3m_io/host name has           | /etc/hosts has',
     ];
 
     /**
